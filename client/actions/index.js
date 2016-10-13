c0b5cb1652c44ed3784de651d2f5ad03f7edd6d3
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2016-10-13 16:56:09 +0200

    get rid of lib folder

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,10 @@
-import componentGenerator from '../../lib/componentGenerator';
-
 export const pumpAction = (action) => {
   return {
     type: 'server/COMMAND',
-    data: componentGenerator('pump', 'actuator', action)
+    data: {
+      name: 'pump',
+      type: 'actuator',
+      value: action
+    }
   };
 };
