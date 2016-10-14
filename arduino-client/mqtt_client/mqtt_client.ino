6817e0193b25940b8887c1f6bf15302728f86b1d
nicor88 <ncorda@babbel.com>
2016-10-14 14:50:05 +0200

    removed libs

--- arduino-client/mqtt_client/mqtt_client.ino
+++ arduino-client/mqtt_client/mqtt_client.ino
@@ -1,6 +1,8 @@
 #include <Bridge.h>
 #include <BridgeClient.h>
 #include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include <string.h>
 
 BridgeClient net;
 MQTTClient client;
@@ -8,11 +10,12 @@ MQTTClient client;
 unsigned long lastMillis = 0;
 int humidityAnalogPin = 0;
 int pumpDigitalPin = 13;
+StaticJsonBuffer<200> jsonBuffer;
  
 void setup() {
   Bridge.begin();
   Serial.begin(9600);
-  client.begin("192.168.240.234", 1883, net);
+  client.begin("192.168.1.201", 1883, net);
   digitalWrite(pumpDigitalPin, HIGH);
   connect();
 }
@@ -37,8 +40,7 @@ void loop() {
   // publish a message roughly every second.
   if(millis() - lastMillis > 1000) {
     lastMillis = millis();
-    String blockMessageOut = createBlockMessage(1,"humidity", analog);
-    Serial.println(blockMessageOut);
+    String blockMessageOut = createBlockMessage(1,"humidity", analogRead(humidityAnalogPin));
     client.publish("arduino/out", blockMessageOut);
   }
 }
@@ -46,17 +48,31 @@ void loop() {
 void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
   if( topic == "arduino/in") {
     Serial.println(payload);
+    JsonObject& received = jsonBuffer.parseObject(payload);
+    const char* param = received["param"];
+    const char* value = received["value"];
+    if(param == "pump_in") {
+      setupPump(value);
+    }
+    Serial.println(payload);
   }
 }
 
 String createBlockMessage(int blockId, String param, int value) {
-  //int value = analogRead(blockIdAnalogPin);
-  String message = "{'blockId':";
-  message += blockId;
-  message += ",'param':'";
-  message += param;
-  message += "','value':'";
-  message += value;
-  message += "'}";
-  return message;
+  JsonObject& message = jsonBuffer.createObject();
+  message["blockId"] = blockId;
+  message["param"] = param;
+  message["value"] = value;
+  String messageString;
+  message.printTo(messageString);
+  return messageString;
+}
+
+void setupPump(String value) {
+  if (value == "1") {
+    digitalWrite(pumpDigitalPin, LOW);
+  }
+  if (value == "0") {
+    digitalWrite(pumpDigitalPin, HIGH);
+  }
 }
